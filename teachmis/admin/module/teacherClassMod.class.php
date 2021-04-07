e65c81b428f61dde1b483878da9092fe21628e51
wanghaijian <wanghaijian3@126.com>
2021-04-07 17:07:58 +0800

    2021-4-7 功能修改 1 我的班级

--- teachmis/admin/module/teacherClassMod.class.php
+++ teachmis/admin/module/teacherClassMod.class.php
@@ -27,7 +27,18 @@ class teacherClassMod extends commonMod
     /**
      * 查看详情
     */
-    public function info(){}
+    public function info()
+    {
+        $id=$_GET['id'];
+        $this->classlist=model('classs')->list_select('');
+        $this->grade=model("grade")->list_select('');
+        $this->list=model('groupinfo')->admin_list();
+        $info=model('users')->info($id);
+        $this->assign("info",$info);
+        $this->action_name='编辑学生';
+        $this->action='edit';
+        $this->show();
+    }
     /**
      * 查看班级成绩曲线
     */
@@ -39,6 +50,21 @@ class teacherClassMod extends commonMod
     /**
      * 查看学生成绩
     */
-    public function getrejected(){}
+    public function getrejected()
+    {
+        $listRows = 20;//每页显示的信息条数
+        $url = __URL__ . '/index/page-{page}.html';//分页基准网址
+        $limit = $this->pagelimit($url, $listRows);
+
+        $where['teacherId']=$_SESSION["user_yg"]["id"];
+        //获取行数
+        $num = model('users')->usersCount($where);
+        //获取信息列表
+        $list = model('users')->usersList($where, $limit);
+        $this->assign('list', $list);
+        $this->menu_name = '学生成绩列表';
+        $this->page = $this->page($url, $num, $listRows);
+        $this->show();
+    }
 }
 ?>
