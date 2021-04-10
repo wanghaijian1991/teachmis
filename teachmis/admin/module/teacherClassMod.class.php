a270d7dcf72a08aaa057caf3dc1e8041989d4205
wanghaijian <wanghaijian3@126.com>
2021-04-11 01:09:03 +0800

    2021-4-10 功能修改 1 我的班级

--- teachmis/admin/module/teacherClassMod.class.php
+++ teachmis/admin/module/teacherClassMod.class.php
@@ -65,7 +65,7 @@ class teacherClassMod extends commonMod
     {
         //引用PHPexcel 类
         include_once(CP_PATH.'PHPExcel/Classes/PHPExcel.php');
-        include_once(CP_PATH.'PHPExcel/Classes/IOFactory.php');//静态类
+        include_once(CP_PATH.'PHPExcel/Classes/PHPExcel/IOFactory.php');//静态类
         $type = 'Excel2007';//设置为Excel5代表支持2003或以下版本，Excel2007代表2007版
         $xlsReader = PHPExcel_IOFactory::createReader($type);
         $xlsReader->setReadDataOnly(true);
